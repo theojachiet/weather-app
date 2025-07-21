e9848bcefdfed4dc8d6a9b1b1ac0aafac47dbff1
Teoslip <tjachiet@gmail.com>
2025-07-21 16:16:04 +0200

    Set up webpack and template files and run a basic fetch request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './reset.css';
+let location = 'auch,FR';
+let url = '';
+
+async function getWeather(location) {
+    url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=54G4W6L2AJ2KDSZXAG9BBLGYG`
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.description);
+}
+
+getWeather(location);
 
 console.log('hello');
