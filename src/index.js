949d392b01c4e7ea2b6c627fc45f0d965c98b1c3
Teoslip <tjachiet@gmail.com>
2025-07-22 17:55:57 +0200

    add svgs and style placeholder content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './reset.css';
+import './style.css';
 let location = 'auch,FR';
 let url = '';
 
@@ -6,9 +7,7 @@ async function getWeather(location) {
     url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=54G4W6L2AJ2KDSZXAG9BBLGYG`
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.description);
+    console.log(data);
 }
 
 getWeather(location);
-
-console.log('hello');
